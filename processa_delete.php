be7120ab1d240b0712772af60cd816d08c067696
Nicolas <nicolas.nog.dias@gmail.com>
2025-06-09 22:43:43 -0300

    Update processa_delete.php

--- processa_delete.php
+++ processa_delete.php
@@ -5,15 +5,24 @@ require_once 'conexao_socars.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
+    if ($id == 1) {
+        echo "<script>
+                alert('Impossível deletar Administrador Principal.');
+                window.location.href = 'lista_admin.php';
+              </script>";
+        exit;
+    }
+
     $stmt = $pdo->prepare("DELETE FROM usuario WHERE id = :id AND acesso = 'admin'");
 
     if ($stmt->execute(['id' => $id])) {
         echo "<script>
                 alert('Administrador removido com sucesso.');
-                window.location.href = 'home.php';
+                window.location.href = 'lista_admin.php';
               </script>";
         exit;
     } else {
         echo "Erro ao excluir o administrador.";
     }
 }
+
