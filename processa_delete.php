5ffa1f42172376f988051ec91629c2e81a876b34
Gabriel <gabrielwilliam234@gmail.com>
2025-06-10 18:48:45 -0300

    Update processa_delete.php

--- processa_delete.php
+++ processa_delete.php
@@ -5,7 +5,7 @@ require_once 'conexao_socars.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    if ($id == 1) {
+    if ($id == 1 || $_SESSION['acesso'] == 'admin_insano') {
         echo "<script>
                 alert('Impossível deletar Administrador Principal.');
                 window.location.href = 'lista_admin.php';
