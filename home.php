91f0d74e1a2e5e17efd39b67cb804fb6f48d2c41
Gabriel <gabrielwilliam234@gmail.com>
2025-06-10 18:47:19 -0300

    Update home.php

--- home.php
+++ home.php
@@ -14,23 +14,35 @@ require 'conexao_socars.php';
 </head>
 <body>
     <h1> Seja Bem vindo! </h1>
-    <?php if(isset($_SESSION['acesso']) && $_SESSION['acesso'] == 'admin'): ?>
+    <?php if(isset($_SESSION['acesso']) && $_SESSION['acesso'] == 'admin'): ?> <!-- ADMINISTRADORES-->
         <h3> O seu acesso é de <?php echo htmlspecialchars($_SESSION['acesso'])?></h3>
         <h3> O seu nome é <?php echo htmlspecialchars($_SESSION['nome'])?></h3>
-        <a href="registro_admin.php"> <button> Cadastro de administradores. </button></a>
+
         <a href="login.php"> <button> Login. </button></a>
         <a href="logout.php"><button> Desconectar. </button> </a>
         <a href="carros_admin.php"><button> Edição de carros. </button></a>
-        <a href="lista_admin.php"><button> Lista de administradores. </button></a>
-    <?php elseif(isset($_SESSION['acesso']) && $_SESSION['acesso'] == 'cliente'): ?>
+
+    <?php elseif(isset($_SESSION['acesso']) && $_SESSION['acesso'] == 'cliente'): ?> <!-- CLIENTES -->
         <h3> O seu acesso é de <?php echo htmlspecialchars($_SESSION['acesso'])?></h3>
         <h3> O seu nome é <?php echo htmlspecialchars($_SESSION['nome'])?></h3>
+
          <a href="carros.php"><button>Carros.</button></a> 
          <a href="logout.php"><button> Desconectar. </button> </a>
-    <?php else: ?>
+
+    <?php elseif (isset($_SESSION['acesso']) && $_SESSION['acesso'] == 'admin_insano'): ?> <!-- ADMINS INSANOS-->
+        <h3> O seu acesso é de <?php echo htmlspecialchars($_SESSION['acesso'])?></h3>
+        <h3> O seu nome é <?php echo htmlspecialchars($_SESSION['nome'])?></h3>
+
         <a href="registro_user.php"><button>Cadastre-se já!</button></a>
         <a href="login.php"> <button> Login. </button></a>
-        <a href="carros.php"><button>Carros.</button></a>
+        <a href="carros_admin.php"><button>Edição de carros.</button></a>
+        <a href="lista_admin.php"><button> Lista de administradores. </button></a>
+        <a href="registro_admin.php"> <button> Cadastro de administradores. </button></a>
+
+    <?php else: ?> <!-- BOBOCAS?-->
+        <a href="login.php"> <button> Login. </button></a>
+        <a href="registro_user.php"><button>Cadastre-se já!</button></a>
+        <a href="carros.php"><button> Carros. </button></a>      
     <?php endif;?>
 
 
