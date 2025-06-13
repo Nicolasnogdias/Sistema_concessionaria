a57894fafa1b5b8225fe12201b528a400ae08d80
Nicolas <nicolas.nog.dias@gmail.com>
2025-06-13 20:23:46 -0300

    verifica_admin.php

--- verifica_admin.php
+++ verifica_admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['acesso']) || $_SESSION['acesso'] !== 'admin' && $_SESSION['acesso'] !== 'admin_insano') {
+if (!isset($_SESSION['acesso']) || $_SESSION['acesso'] !== 'admin' && $_SESSION['acesso'] !== 'admin_principal') {
     echo "<h3>Você não tem permissão para interagir com esta página.</h3>";
     exit;
 }
